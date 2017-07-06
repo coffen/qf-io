a04b1d6f547a7099dd8ae5d0e11c19e83fcf81b5
qiongqi <qiongqi@ixiaopu.com>
2017-07-06 18:41:24 +0800

    添加测试用例

---
+++ src/test/java/com/qf/io/test/excel/PoiExcelWriterTest.java
@@ -0,0 +1,41 @@
+package com.qf.io.test.excel;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.qf.io.csv.SimpleCsvReader;
+
+/**
+ * 
+ * <p>
+ * Project Name: C2C商城
+ * <br>
+ * Description: PoiExcel模板导出测试
+ * <br>
+ * File Name: PoiExcelWriterTest.java
+ * <br>
+ * Copyright: Copyright (C) 2015 All Rights Reserved.
+ * <br>
+ * Company: 杭州偶尔科技有限公司
+ * <br>
+ * @author 穷奇
+ * @create time：2017年7月6日 下午3:01:37 
+ * @version: v1.0
+ *
+ */
+public class PoiExcelWriterTest {
+	
+	private final static Logger log = LoggerFactory.getLogger(SimpleCsvReader.class);
+	
+	@Test
+	public void readExcel() throws Exception {
+
+	}
+	
+	@Test
+	public void readExcelByField() throws Exception {
+
+	}
+	
+}
